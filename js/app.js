d265ee81e0d2a84fabba408c9b1db7ed58a9615f
Saymon Victor <sayminnv88@gmail.com>
2022-07-11 16:34:02 -0300

    adding to the layout search bar and place to filter search

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const fetchPokemon = () => {
 
   const pokemonPromises = []
 
-  for (let i = 1; i <= 200; i++) {
+  for (let i = 1; i <= 174; i++) {
     pokemonPromises.push(
       fetch(getPokemonUrl(i)).then(response => response.json())
     )
